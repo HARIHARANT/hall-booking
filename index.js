35c0e1bdeb7974d2d2e07c87a8e090246327114d
HARIHARANT <hariharancseksr@gmail.com>
2022-05-07 16:04:25 +0530

    Revert "netlify changes"
    This reverts commit 189b03dcfd0f2feb33fa652f2db2cebd0ca41af6.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const serverless = require('serverless-http');
 const mongo = require("./shared");
 const roomsRouter = require("./router/createRoom");
 const bookingsRouter = require("./router/bookings");
 const app = express();
-const router = express.Router();
+
 app.use(express.json());
 mongo.connect();
 
@@ -16,9 +15,4 @@ app.use("/",(req,res,next)=>{
 app.use("/rooms",roomsRouter);
 app.use("/bookings",bookingsRouter);
 
-app.use('/.netlify/functions/server', router);
-
 app.listen("3003");
-
-module.exports = app;
-module.exports.handler = serverless(app);
